a08a54976fcad5d064eb6bcecdfc5e871afa1b32
SHRADDHA S HEGDE <108947381+Shraddhahegde3107@users.noreply.github.com>
2022-08-10 18:26:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,9 +24,8 @@ streamlit.dataframe(fruits_to_show)
 
 #redefined code
 
-def get_fruityvice_data(this_fruit_choice):
-  
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+def get_fruityvice_data(this_fruit_choice):  
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       return fruityvice_normalized
       
@@ -39,8 +38,8 @@ try:
 back_from_function=get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
       
-except URLError as e:
-  streamlit.error()
+#except URLError as e:
+ # streamlit.error()
 
 #import requests
 
